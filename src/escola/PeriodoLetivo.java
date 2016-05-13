5a2d15c3273a51c520d80f18735278fb1dd11a9a
Rafael <Rafael@LAPTOP-K2LUD22V>
2016-05-13 16:15:34 -0300

    Turma atualizada, período letivo criado

---
+++ src/escola/PeriodoLetivo.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escola;
+
+import java.util.List;
+
+/**
+ *
+ * @author Rafael
+ */
+public class PeriodoLetivo {
+    private int ano;
+    private List<Turma> listaTurmas;
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public List<Turma> getListaTurmas() {
+        return listaTurmas;
+    }
+
+    public void setListaTurmas(List<Turma> listaTurmas) {
+        this.listaTurmas = listaTurmas;
+    }
+    
+    
+    
+}
--- src/escola/Turma.java
+++ src/escola/Turma.java
@@ -5,12 +5,42 @@
  */
 package escola;
 
+import Pessoas.Membro;
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author Rafael
  */
 public class Turma {
     
-    
+    private Date data_inicio;
+    private Date data_fim;
+    List<Membro> alunos;
+
+    public Date getData_inicio() {
+        return data_inicio;
+    }
+
+    public void setData_inicio(Date data_inicio) {
+        this.data_inicio = data_inicio;
+    }
+
+    public Date getData_fim() {
+        return data_fim;
+    }
+
+    public void setData_fim(Date data_fim) {
+        this.data_fim = data_fim;
+    }
+
+    public List<Membro> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(List<Membro> alunos) {
+        this.alunos = alunos;
+    }
     
 }
