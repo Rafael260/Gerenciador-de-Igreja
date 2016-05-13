6218dd81a85bf9248bee0333d501b02583460741
Geraldogrj <geraldogrj@gmail.com>
2016-05-13 16:14:56 -0300

    implementado turma 13/05 Geraldo

--- src/escola/Turma.java
+++ src/escola/Turma.java
@@ -1,15 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package escola;
 
 /**
  *
- * @author Rafael
+ * @author Eliseu, Geraldo e Rafael
  */
 public class Turma {
+    
+    protected int id;
+
+    public Turma(int id) {
+        this.id = id;
+    }
     
     
     
